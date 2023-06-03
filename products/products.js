e16263d5674563e277ef6cb0b69621560c73b8ac
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-06-03 19:57:01 +0200

    Onlineshop in netlify

---
+++ products/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: 'iPhone 12 Pro',
+        description: '6.1-inch display',
+        price: 999,
+        image: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677507209/onlineShop/iphone12pro_znesnq.jpg',
+    },
+    {
+        id: 2,
+        name: 'iPhone 12',
+        description: '5.4-inch display',
+        price: 699,
+        image: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677507209/onlineShop/iphone12_twkvr9.jpg',
+    },
+    {
+        id: 3,
+        name: 'Galaxy S',
+        description: '6.5-inch display',
+        price: 399,
+        image: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677507209/onlineShop/galaxyS_f223ev.png',
+    },
+];
+
+module.exports = products;
